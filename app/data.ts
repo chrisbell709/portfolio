622400d22a96d22c8bd7d932e56935929d08f542
chrisbell709 <chrisbell709@gmail.com>
2025-03-09 17:57:28 -0400

    Update data.ts

--- app/data.ts
+++ app/data.ts
@@ -103,16 +103,8 @@ export const SOCIAL_LINKS: SocialLink[] = [
     link: 'https://github.com/ibelick',
   },
   {
-    label: 'Twitter',
-    link: 'https://twitter.com/ibelick',
-  },
-  {
     label: 'LinkedIn',
     link: 'https://www.linkedin.com/in/ibelick',
-  },
-  {
-    label: 'Instagram',
-    link: 'https://www.instagram.com/ibelick',
   },
 ]
 
