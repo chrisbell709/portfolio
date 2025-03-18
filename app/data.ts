03de99a7ade74aeca051c0627697e001e6755092
chrisbell709 <chrisbell709@gmail.com>
2025-03-18 14:36:35 -0400

    Update data.ts

--- app/data.ts
+++ app/data.ts
@@ -29,10 +29,10 @@ type SocialLink = {
 
 export const PROJECTS: Project[] = [
   {
-    name: 'Motion Primitives Pro',
+    name: 'Daily Trivia',
     description:
-      'Advanced components and templates to craft beautiful websites.',
-    link: 'https://pro.motion-primitives.com/',
+      'Daily trivia web app built with AI, React, and Next.JS ',
+    link: 'https://triviaaa.dev',
     video:
       'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
     id: 'project1',
