f6e97356b3748941697a181fe9ea00fe2e952708
chrisbell709 <chrisbell709@gmail.com>
2025-03-21 11:36:35 -0400

    Update data.ts

--- app/data.ts
+++ app/data.ts
@@ -38,9 +38,9 @@ export const PROJECTS: Project[] = [
     id: 'project1',
   },
   {
-    name: 'Motion Primitives',
-    description: 'UI kit to make beautiful, animated interfaces.',
-    link: 'https://motion-primitives.com/',
+    name: 'Top Chef ELO Tracker',
+    description: 'Tracking ELO rankings for Top Chef Season 22',
+    link: 'https://v0-top-chef-elo-tracker.vercel.app/',
     video:
       'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
     id: 'project2',
