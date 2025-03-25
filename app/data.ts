4c59fdd1670930dc1ee7225f81cde4513745b0dc
chrisbell709 <chrisbell709@gmail.com>
2025-03-24 20:11:21 -0400

    Update data.ts

--- app/data.ts
+++ app/data.ts
@@ -39,7 +39,7 @@ export const PROJECTS: Project[] = [
   },
   {
     name: 'Top Chef ELO Tracker',
-    description: 'Tracking ELO rankings for Top Chef Season 22',
+    description: 'Tracking ELO rankings for Top Chef Season 22 built with AI',
     link: 'https://v0-top-chef-elo-tracker.vercel.app/',
     video:
       'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
