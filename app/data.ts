d021c00a73782b6077ea98187df481af4e8c4f05
chrisbell709 <chrisbell709@gmail.com>
2025-07-11 15:30:17 -0400

    Update data.ts

--- app/data.ts
+++ app/data.ts
@@ -39,7 +39,7 @@ export const PROJECTS: Project[] = [
   },
   {
     name: 'diabase.app',
-    description: 'Diabase hold identification game',
+    description: 'Boulder problem identification game',
     link: 'https://diabase.app/',
     video:
       'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
